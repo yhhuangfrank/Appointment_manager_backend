55ce9853da2a14b7cdc634b8bcf6a47b81f650e6
FrankHuang <frank.huang@kooppi.com>
2024-11-14 17:51:58 +0800

    refactor: remove unnecessary model

--- service/service_hosp/src/main/java/com/frank/hosp/repository/HospitalSettingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.frank.hosp.repository;
-
-import com.frank.model.HospitalSetting;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HospitalSettingRepository extends JpaRepository<HospitalSetting, Long> {
-}
